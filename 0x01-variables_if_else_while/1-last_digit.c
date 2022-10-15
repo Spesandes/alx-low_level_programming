d6f0fadae655bcc3a7faf16529e07ef5107db4b9
Spesandes <sspesande@gmail.com>
2022-10-15 02:52:11 -0700

    This program determines if whether n is greater than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, 8);
+		printf("Last digit of %d is %d and is 0\n", n, 0);
 	}
 	else if (n > 5)
 	{
