e7326f3eef5be4e1b41f85c18767b2cb3960db6f
Spesandes <sspesande@gmail.com>
2022-10-16 10:14:22 -0700

    This program prints two digits followed by a comma and a space

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 > 9; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = num1 + 1; num2 > 10; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
@@ -21,6 +21,8 @@ int main(void)
 			putchar(' ');
 		}
 	}
+
 	putchar ('\n');
+
 	return (0);
 }
