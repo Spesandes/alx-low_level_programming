606bf9cf0c401e0806df78993721b4c74d783c7e
Spesandes <sspesande@gmail.com>
2022-10-18 14:27:54 -0700

    This program prints alphabets in lowercases 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10x the alphabets in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	int line = 0, alpha;
+
+	while (line <= 9)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		line++;
+	}
+	;
+}
