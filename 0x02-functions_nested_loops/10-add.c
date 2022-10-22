584767b8e2479f4abf50496bdd915aedec702eea
Spesandes <sspesande@gmail.com>
2022-10-21 23:52:05 -0700

    This function adds two arguments of the function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds to integers of the function
+ * @x: first argument
+ * @y: second argument
+ *
+ * Return: Sum of two numbers
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
