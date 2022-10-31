566b1bdb879e8ab46ee9a5fdad0078e246b85ec8
Spesandes <sspesande@gmail.com>
2022-10-31 05:31:51 -0700

    This function fills the first n bytes of the memory area pointed to by s with the constant byte b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a constant byte
  * @s: pointer to char i.e the address of memory to print
  * @b: constant byte to fill memory area pointed to by s
  * @n: size of the memory to print
- * Return: a pointer to the memory area s
+ * Return: a pointer to the memory area (s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
