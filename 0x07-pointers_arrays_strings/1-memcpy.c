56334cc35ddf178934f054057c121a2cb7d2ecc5
Spesandes <sspesande@gmail.com>
2022-10-31 22:54:08 -0700

    This function copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: char pointer to copy to
+ * @src: char pointer from which to copy
+ * @n: number of elements to copy
+ * Return: pointer to (dest)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 0x07 c. Even more pointers, arrays, and strings
+This function fills the memory with a constant byte
+1-memcpy.c - This function copies memory area
