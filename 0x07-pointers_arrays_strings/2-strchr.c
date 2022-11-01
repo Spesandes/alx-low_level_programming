349baec0a25dbb0c4bd08b8face814662ac99673
Spesandes <sspesande@gmail.com>
2022-11-01 01:16:36 -0700

    This function locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
-		if (*s != c)
-			s++;
-		else
+		if (*s == c)
 			return (s);
-	}
-	if (c == '\0')
-		return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
 
-	return (NULL);
+		s++;
+
+	}
+
+	return (s + 1);
 }
