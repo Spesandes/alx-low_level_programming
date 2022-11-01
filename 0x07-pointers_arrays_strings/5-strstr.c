4e83ed7e8173617e98c3c9c417a1bb11e3d72f4b
Spesandes <sspesande@gmail.com>
2022-11-01 06:29:13 -0700

    This function locates the first occurrence of needle in haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - locates first occurrence of @needle in @haystack
+ * @haystack: pointer to char variable from which to search
+ * @needle: pointer to char variable for which to search
+ * Return: pointer to search for 
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; ++i)
+	{
+		for (j = 0; needle[j]; ++j)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ This function fills the memory with a constant byte
 2-strchr.c - This function locates a character in a string
 3-strspn.c - This function searches a string for a set of bytes
 4-strpbrk.c - This function searches a string for any set of bytes
+5-strstr.c - This function locates first occurrence of needle in haystack
